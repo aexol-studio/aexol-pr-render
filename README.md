e5cbf1363e5bc6d034e21ebea35220e5f745dd60
Krzysiek Biały <krzysiekbialy@Mac-mini-Krzysiek.local>
2024-02-16 15:17:10 +0100

    render first try

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next.js Hello World Aexol
---
+++ components/Header.js
@@ -0,0 +1,15 @@
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 className="title">{title}</h1>
+      <p>Hey there 👋!</p>
+
+      <p>
+        We're <a href="https://aexol.com">Aexol</a>, a technology-first
+        company's best friend. We love building mobile apps, web apps, SaaS
+        tools, open-source & blazingly fast ecommerece solutions based on
+        Vendure!
+      </p>
+    </>
+  );
+}
